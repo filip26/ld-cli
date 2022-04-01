ac18b2938e73e4a8d50ea9f698c5075016972384
Filip <filip26@gmail.com>
2022-04-01 23:45:50 +0200

    Add JSON pretty print support

--- src/main/java/com/apicatalog/cli/ExpandCmd.java
+++ src/main/java/com/apicatalog/cli/ExpandCmd.java
@@ -1,10 +1,18 @@
 package com.apicatalog.cli;
 
+import java.io.StringWriter;
+import java.util.Collections;
 import java.util.concurrent.Callable;
 
 import com.apicatalog.jsonld.JsonLd;
+import com.apicatalog.jsonld.api.ExpansionApi;
 import com.apicatalog.jsonld.document.JsonDocument;
 
+import jakarta.json.Json;
+import jakarta.json.JsonArray;
+import jakarta.json.JsonWriter;
+import jakarta.json.JsonWriterFactory;
+import jakarta.json.stream.JsonGenerator;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Option;
@@ -17,9 +25,12 @@ final class ExpandCmd implements Callable<Integer> {
     @Option(names = { "-h", "--help" }, hidden = true, usageHelp = true)
     boolean help = false;
 
+    @Option(names = { "-p", "--pretty" }, description = "pretty print output JSON")
+    boolean pretty = false;
+    
     @Parameters(index = "0", arity = "0..1", description = "input URL")
     String input;
-
+    
     @Spec
     CommandSpec spec;
 
@@ -29,14 +40,41 @@ final class ExpandCmd implements Callable<Integer> {
     @Override
     public Integer call() throws Exception {
 
+        ExpansionApi api;
+        
         if (input != null) {
             System.out.println("Fetching " + input);
-            System.out.println(JsonLd.expand(input).get().toString());
+            api = JsonLd.expand(input);
 
         } else {
-            System.out.println(JsonLd.expand(JsonDocument.of(System.in)).get().toString());
+            api = JsonLd.expand(JsonDocument.of(System.in));
         }
+        
+        JsonArray output = null;
+        
+        try {
+            output = api.get();
+        
+        } catch (Throwable e) {
+            System.err.println(e.getMessage());
+            return spec.exitCodeOnExecutionException();            
+        }
+        
+        if (pretty) {
+            final JsonWriterFactory writerFactory = Json.createWriterFactory(Collections.singletonMap(JsonGenerator.PRETTY_PRINTING, true));
 
+            StringWriter stringWriter = new StringWriter();
+
+            try (final JsonWriter jsonWriter = writerFactory.createWriter(stringWriter)) {
+                jsonWriter.writeArray(output);
+            }
+            
+            System.out.println(stringWriter.toString());
+            
+        } else {
+            System.out.println(output.toString());            
+        }
+                
         return spec.exitCodeOnSuccess();
     }
 }
